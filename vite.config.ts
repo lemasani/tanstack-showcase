7389dac19a3e3de409f2063a07cf1fe81d73b304
lemasani <brianlemasan@gmail.com>
2025-12-27 06:29:08 +0300

    created project and added tanstack form, router and shadcn ui components

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  
+  resolve: {
+     alias: {
+       "@": path.resolve(__dirname, "./src"),
+     },
+   },
+})
